97980aa8ff13095dca81e8407aa46d125c4dd275
nikita <novikov_nv@icloud.com>
2024-03-05 14:41:04 +0300

    add background

--- app/src/main/java/com/example/millionaire/presentation/finish_game_screen/FinishGameScreen.kt
+++ app/src/main/java/com/example/millionaire/presentation/finish_game_screen/FinishGameScreen.kt
@@ -2,6 +2,7 @@ package com.example.millionaire.presentation.finish_game_screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -39,87 +42,97 @@ fun FinishGameScreen(
 ) {
     val lifecycle = LocalLifecycleOwner.current.lifecycle
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.logo),
-            modifier = Modifier.size(width = 195.dp, height = 195.dp),
-            contentDescription = "logo"
-        )
-        Text(
-            text = _gameOver,
-            color = Color.White,
-            fontSize = 32.sp,
-            fontWeight = FontWeight(600)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(R.drawable.background),
+                contentScale = ContentScale.FillBounds
+            )
+    )
+    {
 
-        Text(
-            text = _levels + "$level",
-            color = Color.White,
-            fontSize = 16.sp,
-            fontWeight = FontWeight(400)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = painterResource(R.drawable.coin),
-                modifier = Modifier.size(width = 32.dp, height = 32.dp),
-                contentDescription = "coin"
+                painter = painterResource(R.drawable.logo),
+                modifier = Modifier.size(width = 195.dp, height = 195.dp),
+                contentDescription = "logo"
             )
-            Spacer(modifier = Modifier.width(10.dp))
             Text(
-                text = "$$countMoney",
+                text = _gameOver,
                 color = Color.White,
-                fontSize = 24.sp,
+                fontSize = 32.sp,
                 fontWeight = FontWeight(600)
             )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = _levels + "$level",
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight(400)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.coin),
+                    modifier = Modifier.size(width = 32.dp, height = 32.dp),
+                    contentDescription = "coin"
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "$$countMoney",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(600)
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                if (lifecycle.currentState == Lifecycle.State.RESUMED) {
+                    navigateToLoginScreen()
+                }
+            }) {
+                Text(
+                    text = _newGame,
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(600)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if (lifecycle.currentState == Lifecycle.State.RESUMED) {
+                    navigateToMainScreen()
+                }
+            }) {
+                Text(
+                    text = _mainScreen,
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(600)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(64.dp))
+
         }
     }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            if (lifecycle.currentState == Lifecycle.State.RESUMED) {
-                navigateToLoginScreen()
-            }
-        }) {
-            Text(
-                text = _newGame,
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight(600)
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            if (lifecycle.currentState == Lifecycle.State.RESUMED) {
-                navigateToMainScreen()
-            }
-        }) {
-            Text(
-                text = _mainScreen,
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight(600)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(64.dp))
-
-    }
-
 }
 

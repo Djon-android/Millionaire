ba324d1437ddab8910f419e335a00f043cf93040
Evgeny Kalashnikov <djoncreate@gmail.com>
2024-03-05 06:40:28 +0300

    Начал реализацию экрана игры

--- app/src/main/java/com/example/millionaire/presentation/game_screen/GameScreen.kt
+++ app/src/main/java/com/example/millionaire/presentation/game_screen/GameScreen.kt
@@ -1,8 +1,35 @@
 package com.example.millionaire.presentation.game_screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.millionaire.R
 
 @Composable
 fun GameScreen() {
-
+    Box(
+        contentAlignment = Alignment.BottomStart,
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.background),
+                contentScale = ContentScale.FillBounds
+            )
+            .statusBarsPadding()
+            .safeContentPadding()
+    ) {
+        Text("first", color = Color.White)
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = null, modifier = Modifier.align(
+            Alignment.Center))
+    }
 }
---
+++ app/src/main/java/com/example/millionaire/presentation/game_screen/GameViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.millionaire.presentation.game_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GameViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
